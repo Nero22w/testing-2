263aad34193968f7615080a6384b00842392d364
unknown <nero1warner@gmail.com>
2022-01-16 12:40:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,10 @@
     <body>
         <h1>hello world</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis eaque deleniti sit, illum molestiae accusantium amet ex possimus voluptatibus, hic officia tenetur ipsum soluta minus. Eos dignissimos reiciendis itaque veritatis? </p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+        </ul>
+
     </body>
 </html>
